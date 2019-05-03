27fb03f8412d37fa0fc8c9abeb2c22acb29ec51d
Hellon Canella <helloncanella@gmail.com>
2019-05-03 14:33:00 -0300

    it is logging instagram

--- playground.js
+++ playground.js
@@ -1,18 +1,45 @@
 import puppeteer from "puppeteer"
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
 
-  await page.goto("https://instagram.com", { waitUntil: "networkidle2" })
+  page.once("load", () => console.log("hello"))
+  await page.goto(
+    "https://www.instagram.com/accounts/login/?source=auth_switcher",
+    { waitUntil: "networkidle0" }
+  )
 
-  try {
-    await page.waitForSelector(".piu")
-  } catch (e) {
-    if (e instanceof puppeteer.errors.TimeoutError) {
-      console.log(e)
-    }
-  }
+  await page.evaluate(number => console.log(1, 2, { foo: "bar" }, number), 5)
 
-  console.log("ki")
-  await browser.close()
+  const compte = await page.$("input[name='username']")
+  await compte.type("helloncanella@gmail.com", { delay: 30 })
+
+  const password = await page.$("input[name='password']")
+  await password.type("amadeus1", { delay: 30 })
+  ;(await page.$("button[type='submit']")).click()
+
+  // page.once("load", async () =>
+
+  const wait = time => new Promise(resolve => setTimeout(resolve, time))
+
+  page.once("domcontentloaded", async () =>
+    (await page.$(".aOOlW.HoLwm")).click()
+  )
+
+  // ;().click()
+
+  // wait(500)
+
+  // // await input.screenshot({ path: "piu.png" })
+  // console.log("hllo")
+  // // const oi = await page.content()
+
+  // // console.log(oi)
+
+  // // const o = await page.$$eval("div", as => as.map(a => a.textContent))
+
+  // // console.log({ o })
+
+  // console.log("ki")
+  // await browser.close()
 })()
